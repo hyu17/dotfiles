37c64c4cd4ebbc5534c604e4642ae4170727b1af
George YU <yumocoding@gmail.com>
2023-04-26 19:09:22 +0800

    dotfiles: shell: Add shell configurations
    Signed-off-by: George YU <yumocoding@gmail.com>

---
+++ shell/aliases.sh
@@ -0,0 +1,15 @@
+# git
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gl="git log --all --graph --decorate"
+alias gll="git log --all --graph --decorate --oneline"
+alias gf="git diff"
+
+# common commands
+alias mv="mv -i"
+alias cp="cp -i"
+alias v="vim"
+alias -="cd -"
+alias ..="cd .."
+alias ../..="cd ../.."
